1b7f803fdce1bde87d3d96f1efea5ece96251b0e
korenBarak <koren2221@gmail.com>
2025-05-24 17:57:17 +0300

    fix pr

--- src/pages/MainPage/FilterBox.style.ts
+++ src/pages/MainPage/FilterBox.style.ts
@@ -4,17 +4,15 @@ const styles = {
         borderRadius: 6,
         maxHeight: "100%",
         marginTop: "1vh",
-        minWidth: "28vw",
+        width: "min-content",
+        maxWidth: "30vw",
     },
     categoriesBox: {
-        width: "22vw",
         Height: "30vh",
         display: "flex",
         flexDirection: "column",
         marginLeft: "1.5vh",
         marginTop: "1vh",
-        overflowY: "scroll",
-        overflowX: "scroll"
     }
   };
   
