04a9f705c726101bf0177418ebdb179c0fae7425
unknown <galinbar2411@gmail.com>
2025-05-29 15:28:07 +0300

    handle go back

--- src/pages/SomePages/somePages.tsx
+++ src/pages/SomePages/somePages.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Items from "./items";
 import Stores from "./Stores";
 import { Item } from "../../types/tag.type";
 import { useNavigate } from "react-router-dom";
 import { PATHS } from "../../constants/routes";
 import { Store } from "../../services/store.service";
+import { IconButton } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
 
 enum Option {
   OnlyStore = "OnlyStore",
@@ -17,19 +19,7 @@ const SomePages = () => {
   const [selectedStores, setSelectedStores] = useState<Store[]>([]);
   const [currIndex, setCurrIndex] = useState<number>(-1);
 
-  const option = Option.Both;
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // TODO - check, have i got option from navigation - if not throw to main
-
-    if (option) {
-      calcNextStep();
-    } else {
-      navigate(PATHS.MAIN);
-    }
-  }, []);
+  const option = Option.OnlyStore;
 
   const calcNextStep = () => {
     if (steps[currIndex + 1].isAvailable) {
@@ -65,7 +55,54 @@ const SomePages = () => {
     { isAvailable: true, component: <></> },
   ];
 
-  return <>{currIndex >= 0 && steps[currIndex].component}</>;
+  const navigate = useNavigate();
+
+  const isBackBtnDisabled = useMemo(
+    () =>
+      ![1, 2].some(
+        (i) => currIndex - i >= 0 && steps[currIndex - i].isAvailable
+      ),
+    [currIndex, option]
+  );
+
+  useEffect(() => {
+    // TODO - check, have i got option from navigation - if not throw to main
+
+    if (option) {
+      calcNextStep();
+    } else {
+      navigate(PATHS.MAIN);
+    }
+  }, []);
+
+  const goBack = () => {
+    if (steps[currIndex - 1].isAvailable) {
+      setCurrIndex((prev) => prev - 1);
+    } else {
+      setCurrIndex((prev) => prev - 2);
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          "&:disabled": {
+            color: "red",
+          },
+        }}
+        onClick={goBack}
+        disabled={isBackBtnDisabled}
+      >
+        <ArrowBack />
+      </IconButton>
+
+      {currIndex >= 0 && steps[currIndex].component}
+    </>
+  );
 };
 
 export default SomePages;
